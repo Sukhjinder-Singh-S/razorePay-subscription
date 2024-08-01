39a8e246be0a0c14b04fb68a3a44491261313ac4
sukhjinder-singh-s <sukhjindersingh80810@gmail.com>
2024-08-01 13:50:01 +0530

    upload to ec2

--- subscriptionModel.js
+++ subscriptionModel.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema
 
 const razorSchema = new Schema({
     customerId: { type: String, required: true },
-    subscriptionId: { type: String, required: true },
+    paymentId: { type: String, required: true },
     status: {
-        type: String, enum: ["created", "authenticated", "active", "pending", "halted", "cancelled", "completed", "expired",], default: "created"
+        type: String, enum: ["created", "authorized", "captured", "refunded", "failed"], default: "created"
     },
     userId: { type: Schema.Types.ObjectId, ref: 'user' } // user model name may vary confirm it
 }, { timestamps: true })
